34577e439dfceba2524c1f7ae2b3f132d8cbe7b5
Luzagar <luzagar.abonner@gmail.com>
2024-01-11 16:37:08 +0100

    Algo + graphique (bug mineurs à corriger)

--- Game_project/MainWindow.xaml.cs
+++ Game_project/MainWindow.xaml.cs
@@ -1,65 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace Game_project
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private int[,] matrice = new int[20, 20];
+        private int[,] matrice = new int[40, 40];
         private Random rand = new Random();
         private int randX;
         private int randY;
-        private int sortieX;
-        private int sortieY;
-        private int cheminX;
-        private int cheminY;
-        Point joueur = new Point(); 
-        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
         private double pourcentageZeroEtDeux;
-
 
         public MainWindow()
         {
-           InitializeComponent();
+            InitializeComponent();
             Init main = new Init();
             main.ShowDialog();
             CreationMatrice(matrice);
-            CreationJoueur(matrice) ;
+            CreationJoueur(matrice);
             CreationChemin(matrice);
             CreationCheminAlternatif(matrice);
-            // Création de la boucle de Jeu
-            //  dispatcherTimer.Tick += GameEngine; 
-            // rafraissement toutes les 16 milliseconds
-            //dispatcherTimer.Interval = TimeSpan.FromMilliseconds(16);
-            // lancement du timer
-            //dispatcherTimer.Start();
-
-
+            AffichageMatrice(matrice);
+            CreateShapes();
         }
-  
 
-        private  void CreationMatrice(int[,] tab) 
+        private void CreationMatrice(int[,] tab)
         {
-            
             for (int i = 0; i < tab.GetLength(0); i++)
             {
                 for (int j = 0; j < tab.GetLength(1); j++)
@@ -68,24 +38,16 @@ namespace Game_project
                     tab[0, i] = 2;
                     tab[i, tab.GetLength(0) - 1] = 2;
                     tab[tab.GetLength(0) - 1, i] = 2;
-                  
-                  
-
-
                 }
-               
             }
-            
         }
+
         private void CreationJoueur(int[,] mat)
         {
             randX = rand.Next(mat.GetLength(0) / 4, mat.GetLength(0) / 4 * 3 - 1);
             randY = rand.Next(mat.GetLength(0) / 4, mat.GetLength(0) / 4 * 3 - 1);
             mat[randX, randY] = 4;
-            joueur.X = randX;
-            joueur.Y = randY; 
-            Console.WriteLine($"Player x:{joueur.X} y:{joueur.Y}\n");
-
+            Console.WriteLine($"Player x:{randX} y:{randY}\n");
         }
 
         private void CreationChemin(int[,] mat)
@@ -102,35 +64,31 @@ namespace Game_project
                         {
                             for (int h = 1; h < mat.GetLength(0) / 2 + 1; h++)
                             {
-                                if (mat[i, j] == 3)
+                                if (mat[i, j] == 3 || mat[i,j] == 4)
                                 {
-                                    break;
+                                    continue;
                                 }
                                 else if (mat[i, j] == 1)
                                 {
                                     ChoixChemin(mat, ref i, ref j);
                                 }
-                                if (h > mat.GetLength(0) / 2 -1)
+                                if (h > mat.GetLength(0) / 2 - 1)
                                 {
                                     mat[i, j] = 3;
                                     fin = true;
-                                    Console.WriteLine($"i:{i} j: {j}");
-
+                                    Console.WriteLine($"Sortie x:{i} y:{j}");
                                 }
                             }
                         }
                     }
-                    
-                   
                 }
                 if (fin)
                 {
-
                     break;
                 }
-
             }
         }
+
         private void CalculPZED(int[,] mat, ref double pourcentageZeroEtDeux)
         {
             double nb2 = 0;
@@ -149,80 +107,29 @@ namespace Game_project
                     }
                 }
             }
-            pourcentageZeroEtDeux = (nb2 + nb0) * 100 /((mat.GetLength(0) + 1) * (mat.GetLength(1) + 1));
+            pourcentageZeroEtDeux = (nb2 + nb0) * 100 / ((mat.GetLength(0) + 1) * (mat.GetLength(1) + 1));
         }
+
         private void CreationCheminAlternatif(int[,] mat)
         {
-            int i,j;
             CalculPZED(mat, ref pourcentageZeroEtDeux);
-            Console.WriteLine(pourcentageZeroEtDeux);
             while (pourcentageZeroEtDeux > 50)
             {
-                for (i = rand.Next(0, mat.GetLength(0) - 1); i < mat.GetLength(0); i++)
+                int i = rand.Next(1, mat.GetLength(0) - 1);
+                int j = rand.Next(1, mat.GetLength(1) - 1);
+
+                if (mat[i, j] == 0)
                 {
-                    for (j = rand.Next(0, mat.GetLength(0)- 1); j < mat.GetLength(1); j++)
-                    {
-                        if (mat[i, j] == 1 || mat[i, j] == 4 || mat[i, j] == 5)
-                        {
-                            for (int k = 0; k < 10; k++)
-                            {
-                                if (mat[i, j] == 1 || mat[i, j] == 4 || mat[i, j] == 5)
-                                {
-                                    ChoixCheminAlternatif(mat, ref i, ref j);
-                                }
-                            }
-                            CalculPZED(mat, ref pourcentageZeroEtDeux);
-                            Console.WriteLine(pourcentageZeroEtDeux);
-                            AfffichageMatrice(matrice);
-                        }
-                    }
+                    mat[i, j] = 5;
+                    CalculPZED(mat, ref pourcentageZeroEtDeux);
                 }
             }
         }
-        private void ChoixCheminAlternatif(int[,] mat, ref int x, ref int y)
-        {
-            bool sans = true;
-            randY = 0;
-            randX = 0;
-            while (mat[x + randX, y + randY] != 0)
-            {
-                randX = rand.Next(0, 5);
-                switch (randX)
-                {
-                    case 0:
-                        randX = 1;
-                        randY = 0;
-                        break;
-                    case 1:
-                        randX = -1;
-                        randY = 0;
-                        break;
-                    case 2:
-                        randX = 0;
-                        randY = 1;
-                        break;
-                    case 3:
-                        randX = 0;
-                        randY = -1;
-                        break;
-                    case 4:
-                        sans = false;
-                        randY = 0;
-                        randX = 0;
-                        break;
-                }
-            }
-            if (sans)
-            {
-                mat[x + randX, y + randY] = 5;
-            }
-            y = y + randY;
-            x = x + randX;
-        }
+
         private void ChoixChemin(int[,] mat, ref int x, ref int y)
         {
-            randY = 0;
-            randX = 0;
+            int randY = 0;
+            int randX = 0;
             while (mat[x + randX, y + randY] != 0)
             {
                 randX = rand.Next(0, 4);
@@ -257,35 +164,107 @@ namespace Game_project
             }
             else
             {
-                mat[x + randX, y + randY] = 3;
+                mat[x + randX, y + randY] = 3; // Utilisez 3 pour représenter le chemin alternatif
             }
             y = y + randY * 2;
             x = x + randX * 2;
         }
 
-        private static void AfffichageMatrice(int[,] mat)
+
+        private static void AffichageMatrice(int[,] mat)
         {
             for (int i = 0; i < mat.GetLength(0); i++)
             {
-               
-                    Console.Write("(");
-                    for (int j = 0; j < mat.GetLength(1); j++)
-                        Console.Write($"{mat[i, j]},");
-                    Console.WriteLine(")");
-
+                Console.Write("(");
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write($"{mat[i, j]},");
+                }
+                Console.WriteLine(")");
             }
-           
-
         }
-        private void GameEngine(object sender, EventArgs e)
+        private void CreateShapes()
         {
+            // Parcourez la matrice et créez les formes en fonction des valeurs
+            for (int i = 0; i < matrice.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrice.GetLength(1); j++)
+                {
+                    Shape shape = null;
 
+                    switch (matrice[i, j])
+                    {
+                        case 0: // Remplacez par un rectangle blanc
+                            shape = new Rectangle
+                            {
+                                Width = 20,
+                                Height = 20,
+                                Fill = Brushes.Black
+                            };
+                            break;
+                        case 1: // Remplacez par un rectangle blanc
+                            shape = new Rectangle
+                            {
+                                Width = 20,
+                                Height = 20,
+                                Fill = Brushes.White
+                            };
+                            break;
+                        case 2: // Remplacez par un rectangle noir
+                            shape = new Rectangle
+                            {
+                                Width = 20,
+                                Height = 20,
+                                Fill = Brushes.Black
+                            };
+                            break;
+                        case 3: // Remplacez par un carré jaune
+                            shape = new Rectangle
+                            {
+                                Width = 20,
+                                Height = 20,
+                                Fill = Brushes.Yellow
+                            };
+                            break;
+                        case 4: // Remplacez par un rectangle rouge
+                            shape = new Rectangle
+                            {
+                                Width = 20,
+                                Height = 20,
+                                Fill = Brushes.Red
+                            };
+                            break;
+                        case 5: // Remplacez par un rectangle blanc
+                            shape = new Rectangle
+                            {
+                                Width = 20,
+                                Height = 20,
+                                Fill = Brushes.White
+                            };
+                            break;
+                        default:
+                            break;
+                    }
 
+                    if (shape != null)
+                    {
+                        // Positionnez la forme dans le Canvas
+                        Canvas.SetLeft(shape, j * 20); // Ajustez la position X
+                        Canvas.SetTop(shape, i * 20); // Ajustez la position Y
+
+                        // Ajoutez la forme au Canvas
+                        LabyrinthCanvas.Children.Add(shape);
+                    }
+                }
+            }
         }
-
-    }
-
-        
-
+                }
 }
+       
+        
+            
+        
+    
+
+    
 
